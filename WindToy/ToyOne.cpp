4636add5e5827807ff1ea4b52cedd8e2d47784ae
windpenguin <windpenguin@gmail.com>
2017-08-16 10:28:13 +0800

    init

---
+++ WindToy/ToyOne.cpp
@@ -0,0 +1,40 @@
+//
+//	<one line to give the program's name and a brief idea of what it does.>
+//	Copyright (C) 2017. WenJin Yu. windpenguin@gmail.com.
+//
+//	Created at 2017/8/14 17:21:27
+//	Version 1.0
+//
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+namespace wind {
+
+void WhatAmountDoubelQuotes()
+{
+	cout << "\"" << endl;
+	cout << '"' << endl;
+
+	cout << int('"') << endl;
+}
+
+} // namespace wind
+
---
+++ WindToy/WindToy.cpp
@@ -0,0 +1,19 @@
+// WindToy.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+#include "ToyOne.h"
+
+using namespace std;
+using namespace wind;
+
+int main()
+{
+	WhatAmountDoubelQuotes();
+
+	system("pause");
+    return 0;
+}
+
